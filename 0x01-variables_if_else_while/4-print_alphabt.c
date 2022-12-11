50ddd98425f405a0f7c5147d2963b3ab61be650a
odusoro paul <odusoropaul@gmil.com>
2022-12-11 15:28:11 -0800

    my new task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
