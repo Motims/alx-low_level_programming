136f10d810b1686be901234dac321ceac82a8900
odusoro paul <odusoropaul@gmil.com>
2023-01-03 01:40:21 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _memset -  fills memory with a constant byte.
- * @s: source string
- * @b: the contant byte for filling
- * @n lenght of buffer
- * Return: new string
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
  */
 
 char *_memset(char *s, char b, unsigned int n)
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-my new read me
+readm
